f24d1bf8b247833cf30ac68e7c6ebfc569ffecf6
zahari <zarchaoz@gmail.com>
2015-03-05 13:35:27 +0200

    Basic glutin example.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate glutin;
+extern crate gl;
+
+
 fn main() {
-    println!("Hello, world!");
+    let window = glutin::Window::new().unwrap();
+
+    unsafe { window.make_current() };
+
+    unsafe {
+        gl::load_with(|symbol| window.get_proc_address(symbol));
+
+        gl::ClearColor(0.4, 0.6, 0.9, 1.0);
+    }
+
+    while !window.is_closed() {
+        window.wait_events();
+
+        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
+
+        window.swap_buffers();
+    }
 }
+
