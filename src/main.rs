820b65e70c131590a4d524d49768b242d4e1e99f
zahari <zarchaoz@gmail.com>
2015-06-23 16:22:06 +0300

    Showing a little rectangle with OpenGL 2 functions.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![allow(unused_must_use)]
 
-extern crate gl;
 extern crate glutin;
 extern crate libc;
 
 use std::ffi::CStr;
 use std::str;
+
+mod gl {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
 
 fn gl_string<'a>(name: u32) -> &'a str {
    unsafe {
@@ -22,20 +25,37 @@ fn print_gl_info(){
 }
 
 fn main() {
-   let window = glutin::Window::new().unwrap();
+   let window = glutin::WindowBuilder::new().with_dimensions(250, 250).with_gl_profile(glutin::GlProfile::Compatibility).build().unwrap();
 
    unsafe { window.make_current() };
 
-   unsafe {
-      gl::load_with(|symbol| window.get_proc_address(symbol));
+   println!("Pixel format of the window: {:?}", window.get_pixel_format());
 
-      gl::ClearColor(0.0, 1.0, 0.0, 1.0);
+   gl::load(&window);
+
+   unsafe {
+         gl::ClearColor(1., 1., 1., 1.);
+         gl::Color3f(0., 1., 0.);
+         gl::PointSize(10.);
+         gl::MatrixMode(gl::PROJECTION);
+         gl::LoadIdentity();
+         gl::Ortho(0., 250., 0., 250., -1., 1.);
    }
 
    print_gl_info();
 
    for event in window.wait_events() {
-      unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
+      unsafe {
+         gl::Clear(gl::COLOR_BUFFER_BIT);
+
+         gl::Begin(gl::POINTS);
+         gl::Vertex2i(40, 210);
+         gl::End();
+         gl::Flush();
+
+         gl::Flush();
+      };
+
       window.swap_buffers();
 
       match event {
